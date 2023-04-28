94fb0d2d954db0e92a74f2fa7e36b7a2d00628ba
Gregory Raiz <503720+graiz@users.noreply.github.com>
2023-04-28 10:53:35 -0400

    Added multiple image sprite sheet support.

--- convert.py
+++ convert.py
@@ -11,8 +11,8 @@ except:
     sys.exit(1)
 
 # Get the filename without the extension
-filename = sys.argv[1].split('.')[0]
-
+filename = sys.argv[1].split('.')[0]   
+filename = filename.rsplit('/', 1)[-1].rsplit('.', 1)[0]
 
 # Convert the image to RGB mode
 rgb_im = image.convert('RGB')
@@ -20,30 +20,30 @@ rgb_im = image.convert('RGB')
 # Get the image width and height
 width, height = rgb_im.size
 
-# Create an empty list to store the pixel values
-pixel_values = []
+# Calculate the number of frames
+frames = width // height
 
-r, g, b = rgb_im.getpixel((0, 0))
-#print("red :" + str(r))
-#print(str(int(f"{r:02x}{g:02x}", 16)))
+# Iterate through each frame
+for frame in range(frames):
+    # Create an empty list to store the pixel values
+    pixel_values = []
 
+    # Loop through the image pixels and get the RGB values for the current frame
+    for y in range(height):
+        row = []
+        for x in range(frame * height, (frame + 1) * height):
+            r, g, b = rgb_im.getpixel((x, y))
+            hexrgb = rgb2hex(r, g, b)
+            row.append("0x" + hexrgb[1:])
+        if ((y % 2) == 0):
+            row = list(reversed(row))
+        pixel_values = pixel_values + row
 
-# Loop through the image pixels and get the RGB values
-for y in range(height):
-    row = []
-    for x in range(width):
-        r, g, b = rgb_im.getpixel((x, y))
-        hexrgb = rgb2hex(r,g,b)
-        row.append("0x" + hexrgb[1:])
-    if ((y % 2) == 0):
-        row = list(reversed(row))
-    pixel_values = pixel_values + row 
+    # Print the list of pixel values for the current frame
+    print("const long " + filename + str(frame) + "[] PROGMEM = {", end='')
+    for i, z in enumerate(pixel_values):
+        print(z, end='')
+        if i < len(pixel_values) - 1:
+            print(',', end='')
 
-# Print the list of pixel values
-print("const long " + filename + "[] PROGMEM = {", end='')
-for i, z in enumerate(pixel_values):
-    print(z, end='')
-    if i < len(pixel_values) - 1:
-        print(',', end='')
-
-print("};")
+    print("};")
